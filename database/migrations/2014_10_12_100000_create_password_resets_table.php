ef969428c9078ccb8223bf8cc6a6703e38bc0aa9
David Fernando Torres Zapata <fernando.zapata.live@gmail.com>
2022-11-02 16:16:03 -0500

    Add scafolding vue and auth sistem

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -6,19 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
 
-	public function up()
-	{
-		Schema::create('password_resets', function (Blueprint $table) {
-			$table->string('email')->index();
-			$table->string('token');
-			$table->timestamp('created_at')->nullable();
-		});
-	}
-
-
-	public function down()
-	{
-		Schema::dropIfExists('password_resets');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('password_resets');
+    }
 };
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp } from 'vue'
+
+// Components ---------------------------------------------------
+import ExampleComponent from './components/ExampleComponent.vue'
+
+const app = createApp({
+	components: {
+		ExampleComponent
+	}
+})
+
+app.mount('#app')
